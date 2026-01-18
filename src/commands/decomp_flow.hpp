306c1f877b16827e054073bbc99a63bd5c91d277
yujning <1329882748@qq.com>
2026-01-18 03:00:32 -0500

    将dec -a -d添加了stpdsd方法

--- src/commands/decomp_flow.hpp
+++ src/commands/decomp_flow.hpp
@@ -31,48 +31,80 @@ public:
     add_flag( "--noMux, -u", "don't ues Mux,default = true" );
     add_flag( "--non-dsd, -n", "stop decomposition when func is non-dsd, default = false" );
     add_flag( "--shortsupport, -s", "The decomposition stops when the number of support <= 4, default = false " );
+    add_flag( "--xmg", "convert klut to xmg" );
+    add_flag( "--aig", "convert klut to aig" );
+    add_flag( "--stp_dsd, -d", "use strong dsd with strong_else_dec in stp" );
   }
 
 protected:
-  void execute()
-  {
-    mockturtle::klut_network ntk;
-    std::vector<mockturtle::klut_network::signal> children;
-    kitty::dynamic_truth_table remainder;
-    mockturtle::decomposition_flow_params ps;
+void execute()
+{
+  mockturtle::klut_network ntk;
+  std::vector<mockturtle::klut_network::signal> children;
+  kitty::dynamic_truth_table remainder;
+  mockturtle::decomposition_flow_params ps;
 
-    if ( is_set( "--tt_from_hex" ) )
-    {
-      mockturtle::read_hex( tt_in, remainder, var_num, ntk, children );
-    }
-    else if ( is_set( "--tt_from_binary" ) )
-    {
-      mockturtle::read_binary( tt_in, remainder, var_num, ntk, children );
-    }
-    if ( is_set( "noM3aj" ) )
-    {
-      ps.allow_m3aj = false;
-    }
-    if ( is_set( "noXor" ) )
-    {
-      ps.allow_xor = false;
-    }
-    if ( is_set( "noMux" ) )
-    {
-      ps.allow_mux = false;
-    }
-    if ( is_set( "shortsupport" ) )
-    {
-      ps.allow_s = true;
-    }
-    if ( is_set( "non-dsd" ) )
-    {
-      ps.allow_n = true;
-    }
-    ntk.create_po( mockturtle::dsd_detail( ntk, remainder, children, ps ) );
-    store<klut_network>().extend();
-    store<klut_network>().current() = cleanup_dangling( ntk );
+  /* 1. 读取真值表，只创建 PI */
+  if ( is_set( "--tt_from_hex" ) )
+  {
+    mockturtle::read_hex( tt_in, remainder, var_num, ntk, children );
   }
+  else if ( is_set( "--tt_from_binary" ) )
+  {
+    mockturtle::read_binary( tt_in, remainder, var_num, ntk, children );
+  }
+
+  /* 2. 设置 dsd_detail 参数（仅在 non-STP 情况下使用） */
+  if ( is_set( "noM3aj" ) ) ps.allow_m3aj = false;
+  if ( is_set( "noXor" ) )  ps.allow_xor  = false;
+  if ( is_set( "noMux" ) )  ps.allow_mux  = false;
+  if ( is_set( "shortsupport" ) ) ps.allow_s = true;
+  if ( is_set( "non-dsd" ) )      ps.allow_n = true;
+
+  /* 3. 构建 klut 网络（核心分支） */
+  if ( is_set( "stp_dsd" ) )
+  {
+    also::stp_dsd_lut_resynthesis<klut_network> resyn;
+    klut_network::signal root{};
+
+    resyn(
+      ntk,
+      remainder,
+      children.begin(),
+      children.end(),
+      [&]( klut_network::signal const& s ) {
+        root = s;
+      } );
+
+    ntk.create_po( root );
+  }
+  else
+  {
+    ntk.create_po(
+      mockturtle::dsd_detail( ntk, remainder, children, ps ) );
+  }
+
+  /* 4. 图类型转换（必须在网络建好之后） */
+  if ( is_set( "xmg" ) )
+  {
+    xmg_network xmg = convert_klut_to_graph<xmg_network>( ntk );
+    store<xmg_network>().extend();
+    store<xmg_network>().current() = cleanup_dangling( xmg );
+    return;
+  }
+
+  if ( is_set( "aig" ) )
+  {
+    aig_network aig = convert_klut_to_graph<aig_network>( ntk );
+    store<aig_network>().extend();
+    store<aig_network>().current() = cleanup_dangling( aig );
+    return;
+  }
+
+  /* 5. 默认存 klut */
+  store<klut_network>().extend();
+  store<klut_network>().current() = cleanup_dangling( ntk );
+}
 
 private:
   std::string tt_in;
